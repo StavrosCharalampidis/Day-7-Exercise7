db63f56c4d108435f6d68d56513a16881fa58c98
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-02-09 01:19:46 +0200

    (Try advance print ) Complete

---
+++ ex7.py
@@ -0,0 +1,22 @@
+# It is gonna Print 10 times the hashtag
+print("##########")
+
+# The Variable x stores a string and prints it in f-string format
+x = "Name"
+print("The {} of the user".format(x))
+
+# It is gonna Print 10 times the # but in print you write it (#) 1 time 
+print("#" * 10)
+
+# The Variable firstVar stores a string
+firstVar = "JavaScript"
+
+# The Variable secondVar stores a string
+secondVar = "Is"
+
+# The Variable thirdVar stores a string
+thirdVar = "Bad"
+
+# The end (Variable) in print leaves a space in those prints
+print(firstVar, end = ' ')
+print(secondVar, thirdVar)
